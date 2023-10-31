9f17e1cca02e596e594ee6d2f89276565108b735
jsndz <jaisondz9360.com>
2023-10-31 13:22:41 +0530

    removeitem working

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ app.get("/api/todos", async (req, res) => {
   try {
     console.log("reached 2");
     const todos = await Todo.find();
-    // console.log(todos)
     res.status(201).json({
       message: "sucessfully  found todo",
       data: todos,
@@ -40,13 +39,11 @@ app.get("/api/todos", async (req, res) => {
   }
 });
 app.post("/api/todos", async (req, res) => {
+  console.log(req.body._id)
   const { todoItem } = req.body;
-  console.log(req.body)
   try {
-    // const todo = new Todo({ content });
     const todo = await Todo.create({content:todoItem})
-    console.log(todo)
-    // await todo.save();
+    console.log(req.body)
     res.status(201).json({
       message: "sucessfully  created todo",
       data: todo,
@@ -62,6 +59,26 @@ app.post("/api/todos", async (req, res) => {
     });
   }
 });
+
+app.delete("/api/todos",async (req,res)=>{
+  try {
+    console.log(req.body._id)
+    const response = await Todo.findByIdAndRemove(req.body._id)
+    res.status(201).json({
+      message: "sucessfully  deleted todo",
+      data: response,
+      sucess: true,
+      err: {},
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "couldnt delete todo",
+      data: {},
+      sucess: false,
+      err: { error },
+    });
+  } 
+})
 
 
 app.listen(3001, async () => {
